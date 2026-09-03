ee1a2d352fc1447166f0d6d50a61588f1fdd5b0e
BERNARDO PINHO DE ALMEIDA <026.079806@universidadesenaicimatec.edu.br>
2026-09-03 11:56:38 -0300

    improved menu

--- src/app/componentes/menu/menu.ts
+++ src/app/componentes/menu/menu.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
+  isShown: boolean = false;
 
+  showMenu() {
+    this.isShown = !this.isShown;
+  }
 }
